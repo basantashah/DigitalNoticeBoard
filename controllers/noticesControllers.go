37bb62f5100d30a00d21b0866c8a68e430a21ca2
basantashah <bindassbasanta@hotmail.com>
2019-04-20 19:12:38 +0545

    adding notice controller

--- controllers/noticesControllers.go
+++ controllers/noticesControllers.go
@@ -7,14 +7,14 @@ import (
 	"net/http"
 )
 
-var CreateContact = func(w http.ResponseWriter, r *http.Request) {
+var CreateNotice = func(w http.ResponseWriter, r *http.Request) {
 
 	user := r.Context().Value("user").(uint) //Grab the id of the user that send the request
 	contact := &models.Notices{}
 
 	err := json.NewDecoder(r.Body).Decode(contact)
 	if err != nil {
-		u.Respond(w, u.Message(false, "Error while decoding request body"))
+		u.Respond(w, u.Message(false, "Error while decoding request body, create notice error in notice controller"))
 		return
 	}
 
@@ -23,8 +23,11 @@ var CreateContact = func(w http.ResponseWriter, r *http.Request) {
 	u.Respond(w, resp)
 }
 
-var GetContactsFor = func(w http.ResponseWriter, r *http.Request) {
+var GetNoticeFor = func(w http.ResponseWriter, r *http.Request) {
+	//Allow CORS here By * or specific origin
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	id := r.Context().Value("user").(uint)
 	data := models.Getnotices(id)
 	resp := u.Message(true, "success")
