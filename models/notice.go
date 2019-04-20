69e1e4aca037ab2c68828a52ae7daf298c189f5e
basantashah <bindassbasanta@hotmail.com>
2019-04-20 19:13:56 +0545

    adding more fields in notices

--- models/notice.go
+++ models/notice.go
@@ -3,6 +3,7 @@ package models
 import (
 	"fmt"
 	u "go-contacts/utils"
+	"time"
 
 	"github.com/jinzhu/gorm"
 )
@@ -13,12 +14,16 @@ import (
 // Notices for creating notice table on db
 type Notices struct {
 	gorm.Model
-	Subject    string `json:"subject"`
-	Content    string `json:"content"`
-	Department string `json:"department"`
-	Urgent     string `json:"urgent"`
-	Status     string `json:"status"`
-	UserID     uint   `json:"user_id"`
+	Title string `json:"title"`
+	// Date       time.Time `json:"date"`
+	Expiry     time.Time `json:"expiry"`
+	Subject    string    `json:"subject"`
+	Content    string    `json:"content"`
+	Department string    `json:"department"`
+	Urgent     bool      `json:"urgent"`
+	Status     bool      `json:"status"`
+
+	UserID uint `json:"user_id"`
 }
 
 /*
@@ -27,6 +32,9 @@ type Notices struct {
 returns message and true if the requirement is met
 */
 func (notice *Notices) Validate() (map[string]interface{}, bool) {
+	// if notice.Date < time.Now() {
+	// 	return u.Message(false, "time cannot be in past"), false
+	// }
 
 	if notice.Subject == "" {
 		return u.Message(false, "Subject information should be on the payload"), false
@@ -39,13 +47,16 @@ func (notice *Notices) Validate() (map[string]interface{}, bool) {
 	if notice.Department == "" {
 		return u.Message(false, "Notice from which department should be on payload"), false
 	}
-	if notice.Urgent == "" {
-		return u.Message(false, "The notice type:Urgent or Normal cannot"), false
-	}
+	// if notice.Urgent == "" {
+	// 	return u.Message(false, "The notice type:Urgent or Normal cannot be left empty"), false
+	// }
 
-	if notice.Status == "" {
-		return u.Message(false, "The active or inactive of notice should be there"), false
-	}
+	// if notice.Status !=  {
+	// 	return u.Message(false, "The active or inactive of notice should be there"), false
+	// }
+	// if notice.Expiry == time.Time {
+	// 	return u.Message(false, "Subject information should be on the payload"), false
+	// }
 
 	if notice.UserID <= 0 {
 		return u.Message(false, "User is not recognized"), false
