524c5796de8ab1b2e09f760bd4972240ef04bda1
basantashah <bindassbasanta@hotmail.com>
2019-04-20 19:13:01 +0545

    solving CORS issue

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"go-contacts/app"
 	"go-contacts/controllers"
 	"log"
 	"net/http"
+	"net/http/httputil"
 	"os"
 
-	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
 func main() {
@@ -17,21 +19,55 @@ func main() {
 
 	router.HandleFunc("/api/user/new", controllers.CreateAccount).Methods("POST")
 	router.HandleFunc("/api/user/login", controllers.Authenticate).Methods("POST")
-	router.HandleFunc("/api/notice/post", controllers.CreateContact).Methods("POST")
-	router.HandleFunc("/api/notice/fetch", controllers.GetContactsFor).Methods("GET")
+	router.HandleFunc("/api/notice/post", controllers.CreateNotice).Methods("POST")
+	router.HandleFunc("/api/notice/fetch", controllers.GetNoticeFor).Methods("GET")
 
 	router.Use(app.JwtAuthentication) //attach JWT auth middleware
+	router.Use(LoggingMiddleware)
 
 	port := os.Getenv("PORT")
 
 	if port == "" {
-		port = "8080"
+		port = "8000"
 	}
 
 	log.Println("Listening .......")
 
-	corsObj := handlers.AllowedOrigins([]string{"*"})
+	corsOpts := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"}, //you service is available and allowed for this base url
+		AllowedMethods: []string{
+			http.MethodGet, //http methods for your app
+			http.MethodPost,
+			http.MethodPut,
+			http.MethodPatch,
+			http.MethodDelete,
+			http.MethodOptions,
+			http.MethodHead,
+		},
 
-	log.Fatal(http.ListenAndServe(":"+port, handlers.CORS(corsObj)(router)))
+		AllowedHeaders: []string{
+			"*", //or you can your header key values which you are using in your application
 
+		},
+	})
+
+	http.ListenAndServe(":"+os.Getenv("PORT"), corsOpts.Handler(router))
+}
+
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Do stuff here
+		fmt.Println()
+		log.Println("..............")
+		// if os.Getenv("DUMP_REQUEST") == "true" {
+		requestDump, err := httputil.DumpRequest(r, true)
+		if err != nil {
+			fmt.Println(err)
+		}
+		// }
+		fmt.Println(string(requestDump))
+
+		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		next.ServeHTTP(w, r)
+	})
 }
