2dd4bb92baea5f13d2b0218aa42de802c7d282c3
basantashah <bindassbasanta@hotmail.com>
2019-04-22 22:47:22 +0545

    adding update and delete function

--- controllers/noticesControllers.go
+++ controllers/noticesControllers.go
@@ -11,26 +11,18 @@ import (
 var CreateNotice = func(w http.ResponseWriter, r *http.Request) {
 
 	user := r.Context().Value("user").(uint) //Grab the id of the user that send the request
-	contact := &models.Notices{}
+	notice := &models.Notices{}
 
-	err := json.NewDecoder(r.Body).Decode(contact)
+	err := json.NewDecoder(r.Body).Decode(notice)
 	if err != nil {
 		u.Respond(w, u.Message(false, "Error while decoding request body, create notice error in notice controller"))
 		return
 	}
 
-	contact.UserID = user
-	resp := contact.Create()
+	notice.UserID = user
+	resp := notice.Create()
 	u.Respond(w, resp)
 }
-
-// var GetNoticeFor = func(w http.ResponseWriter, r *http.Request) {
-// 	status := r.Context().Value(true).(bool)
-// 	data := models.Getnotices(status)
-// 	resp := u.Message(true, "success")
-// 	resp["data"] = data
-// 	u.Respond(w, resp)
-// }
 
 var GetNoticeFor = func(w http.ResponseWriter, r *http.Request) {
 	id := r.Context().Value("user").(uint)
@@ -40,6 +32,16 @@ var GetNoticeFor = func(w http.ResponseWriter, r *http.Request) {
 	resp["data"] = data
 	u.Respond(w, resp)
 }
+
+// var DeleteNotice = func(w http.ResponseWriter, r *http.Request) {
+// 	id := r.Context().Value("user").(uint)
+// 	fmt.Println(id)
+// 	data := models.DeleteNotice(id)
+// 	resp := u.Message(true, "success")
+// 	resp["data"] = data
+// 	u.Respond(w, resp)
+
+// }
 
 // only get notice that user posted
 var GetYourNoticesOnly = func(w http.ResponseWriter, r *http.Request) {
@@ -51,18 +53,19 @@ var GetYourNoticesOnly = func(w http.ResponseWriter, r *http.Request) {
 }
 
 // TOp-DO
-// var UpdateNotice = func(w http.ResponseWriter, r *http.Request) {
+var UpdateNotice = func(w http.ResponseWriter, r *http.Request) {
 
-// 	user := r.Context().Value("user").(uint) //Grab the id of the user that send the request
-// 	notice := &models.Notices{}
+	user := r.Context().Value("user").(uint) //Grab the id of the user that send the request
+	notice := &models.Notices{}
 
-// 	err := json.NewDecoder(r.Body).Decode(notice)
-// 	if err != nil {
-// 		u.Respond(w, u.Message(false, "Error while decoding request body, create notice error in notice controller"))
-// 		return
-// 	}
+	err := json.NewDecoder(r.Body).Decode(notice)
+	if err != nil {
+		u.Respond(w, u.Message(false, "Error while decoding request body, create notice error in notice controller"))
+		return
+	}
 
-// 	notice.UserID = user
-// 	resp := notice.Update()
-// 	u.Respond(w, resp)
-// }
+	notice.UserID = user
+	resp := notice.Update()
+	u.Respond(w, resp)
+
+}
